60638c9994a51bddd59f47aa63b2a99627a307c9
cs-emanuelle-menali <emanuelle.menali@concrete.com.br>
2017-11-02 19:48:56 -0200

    Initial commit

---
+++ app/src/main/java/com/example/emanuellemenali/mybeatboxwork/view/activity/BeatBoxActivity.java
@@ -0,0 +1,15 @@
+package com.example.emanuellemenali.mybeatboxwork.view.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.emanuellemenali.mybeatboxwork.R;
+
+public class BeatBoxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beat_box);
+    }
+}
---
+++ app/src/main/java/com/example/emanuellemenali/mybeatboxwork/view/fragment/BeatBoxFragment.java
@@ -0,0 +1,8 @@
+package com.example.emanuellemenali.mybeatboxwork.view.fragment;
+
+/**
+ * Created by emanu on 02/11/2017.
+ */
+
+public class BeatBoxFragment {
+}
