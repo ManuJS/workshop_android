d4ef500cf16f78ab467f9123200df0fd6690d90a
cs-emanuelle-menali <emanuelle.menali@concrete.com.br>
2017-11-02 20:34:59 -0200

    Comecamos a usar o Databinding
    Exibicao do RecyclerView atravÃs do databiding.

--- app/src/main/java/com/example/emanuellemenali/mybeatboxwork/view/fragment/BeatBoxFragment.java
+++ app/src/main/java/com/example/emanuellemenali/mybeatboxwork/view/fragment/BeatBoxFragment.java
@@ -1,10 +1,28 @@
 package com.example.emanuellemenali.mybeatboxwork.view.fragment;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.emanuellemenali.mybeatboxwork.R;
+import com.example.emanuellemenali.mybeatboxwork.databinding.FragmentBeatBoxBinding;
 
 public class BeatBoxFragment extends Fragment {
 
     public static BeatBoxFragment newInstance(){
         return new BeatBoxFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        FragmentBeatBoxBinding binding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_beat_box, container, false);
+        return binding.getRoot();
     }
 }
