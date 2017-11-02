84251c66f5867baf6cb065151ec1ef95d2765687
cs-emanuelle-menali <emanuelle.menali@concrete.com.br>
2017-11-02 21:02:41 -0200

    Primeira etapa do adapter
    Como nÃo estamos trabalhando essa classe dentro do fragmente, precisamos
    passar alguns parametros para o adapter atravÃs do construtor.
    Lista de sons, o contexto da aplicaÃÃo.

---
+++ app/src/main/java/com/example/emanuellemenali/mybeatboxwork/model/Sound.java
@@ -0,0 +1,8 @@
+package com.example.emanuellemenali.mybeatboxwork.model;
+
+/**
+ * Created by emanu on 02/11/2017.
+ */
+
+public class Sound {
+}
---
+++ app/src/main/java/com/example/emanuellemenali/mybeatboxwork/view/adapter/SoundAdapter.java
@@ -0,0 +1,45 @@
+package com.example.emanuellemenali.mybeatboxwork.view.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.example.emanuellemenali.mybeatboxwork.model.BeatBox;
+import com.example.emanuellemenali.mybeatboxwork.model.Sound;
+
+import java.util.List;
+
+/**
+ * Created by emanu on 02/11/2017.
+ */
+
+public class SoundAdapter extends RecyclerView.Adapter<SoundHolder> {
+
+    private List<Sound> mSounds;
+
+    private Context mContext;
+
+    private BeatBox mBeatBox;
+
+    public SoundAdapter(List<Sound> mSounds, Context mContext, BeatBox mBeatBox) {
+        this.mSounds = mSounds;
+        this.mContext = mContext;
+        this.mBeatBox = mBeatBox;
+    }
+
+    @Override
+    public SoundHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(SoundHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
