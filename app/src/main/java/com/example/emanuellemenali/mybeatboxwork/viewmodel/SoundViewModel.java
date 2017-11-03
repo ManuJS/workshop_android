5c085a60fc157ec57dd925a81b26453d9487126d
cs-emanuelle-menali <emanuelle.menali@concrete.com.br>
2017-11-02 23:16:39 -0200

    Notificando a view que existem mudanÃas
    Quando criamos o SoundViewModel, nÃo informavamos a camada de view que
    existiam mudanÃas. EntÃo quando rolavamos a tela da activity a lista de
    musicas mudava de ordem a todo momento. EntÃo agora ele mantÃm a ordem
    da lista.

--- app/src/main/java/com/example/emanuellemenali/mybeatboxwork/viewmodel/SoundViewModel.java
+++ app/src/main/java/com/example/emanuellemenali/mybeatboxwork/viewmodel/SoundViewModel.java
@@ -1,5 +1,8 @@
 package com.example.emanuellemenali.mybeatboxwork.viewmodel;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
 import com.example.emanuellemenali.mybeatboxwork.model.BeatBox;
 import com.example.emanuellemenali.mybeatboxwork.model.Sound;
 
@@ -7,7 +10,7 @@ import com.example.emanuellemenali.mybeatboxwork.model.Sound;
  * Created by emanu on 02/11/2017.
  */
 
-public class SoundViewModel {
+public class SoundViewModel extends BaseObservable{
 
     private Sound mSound;
     private BeatBox mBeatBox;
@@ -16,15 +19,18 @@ public class SoundViewModel {
         this.mBeatBox = mBeatBox;
     }
 
+    @Bindable
+    public String getTitle(){
+        return mSound.getName();
+    }
     public Sound getmSound(){
         return mSound;
     }
 
     public void setmSound(Sound sound){
         mSound = sound;
+        notifyChange();
     }
 
-    public String getTitle(){
-        return mSound.getName();
-    }
+
 }
