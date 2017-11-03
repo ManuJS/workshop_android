5229aee2e1b77cfacde1448411fe9316d93746f3
cs-emanuelle-menali <emanuelle.menali@concrete.com.br>
2017-11-02 22:45:12 -0200

    CriaÃÃo do ViewModel
    Essa Ã© a classe que farÃ¡ a comunicaÃÃo entre o modelo e a activity
    para que sejam executados os sons.

---
+++ app/src/main/java/com/example/emanuellemenali/mybeatboxwork/viewmodel/SoundViewModel.java
@@ -0,0 +1,26 @@
+package com.example.emanuellemenali.mybeatboxwork.viewmodel;
+
+import com.example.emanuellemenali.mybeatboxwork.model.BeatBox;
+import com.example.emanuellemenali.mybeatboxwork.model.Sound;
+
+/**
+ * Created by emanu on 02/11/2017.
+ */
+
+public class SoundViewModel {
+
+    private Sound mSound;
+    private BeatBox mBeatBox;
+
+    public SoundViewModel(BeatBox mBeatBox) {
+        this.mBeatBox = mBeatBox;
+    }
+
+    public Sound getmSound(){
+        return mSound;
+    }
+
+    public void setmSound(Sound sound){
+        mSound = sound;
+    }
+}
