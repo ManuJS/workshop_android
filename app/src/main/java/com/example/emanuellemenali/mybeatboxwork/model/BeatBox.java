ad6d45e27cb0546bd1ecdb3fcc4ee78c9d9ef6da
cs-emanuelle-menali <emanuelle.menali@concrete.com.br>
2017-11-02 22:24:33 -0200

    Comecando a criar uma lista de sons
    Criando a lista de sons para que a classe beatbox execute os itens dessa
    lista.

--- app/src/main/java/com/example/emanuellemenali/mybeatboxwork/model/BeatBox.java
+++ app/src/main/java/com/example/emanuellemenali/mybeatboxwork/model/BeatBox.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.res.AssetManager;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BeatBox {
 
@@ -11,6 +13,7 @@ public class BeatBox {
     private static final String SOUNDS_FOLDER = "sample_sounds";
 
     private AssetManager mAssets;
+    private List<Sound> mSounds = new ArrayList<>();
 
     public BeatBox(Context context) {
         mAssets = context.getAssets();
@@ -26,6 +29,16 @@ public class BeatBox {
         } catch (IOException e) {
             return;
         }
+
+        for (String filename : soundNames){
+            String assetPath = SOUNDS_FOLDER + "/" + filename;
+            Sound sound = new Sound(assetPath);
+            mSounds.add(sound);
+        }
+    }
+
+    public List<Sound> getSounds(){
+        return mSounds;
     }
 
 
--- app/src/main/java/com/example/emanuellemenali/mybeatboxwork/model/Sound.java
+++ app/src/main/java/com/example/emanuellemenali/mybeatboxwork/model/Sound.java
@@ -1,8 +1,16 @@
 package com.example.emanuellemenali.mybeatboxwork.model;
 
-/**
- * Created by emanu on 02/11/2017.
- */
-
 public class Sound {
+    private String mAssetPath;
+    private String name;
+
+    public Sound(String mAssetPath) {
+        this.mAssetPath = mAssetPath;
+        String[] components = mAssetPath.split("/");
+        String fileName = components[components.length-1];
+        this.name = fileName;
+    }
+
+
+
 }
