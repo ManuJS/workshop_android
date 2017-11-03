09ab68b85fc24727e71c8f76213f9190a6c69de9
cs-emanuelle-menali <emanuelle.menali@concrete.com.br>
2017-11-02 22:05:21 -0200

    Metodo para carregar os sons
    Esse metodo cria uma lista com os nomes dos sons que estÃo na pasta de
    assets.

--- app/src/main/java/com/example/emanuellemenali/mybeatboxwork/model/BeatBox.java
+++ app/src/main/java/com/example/emanuellemenali/mybeatboxwork/model/BeatBox.java
@@ -3,6 +3,8 @@ package com.example.emanuellemenali.mybeatboxwork.model;
 import android.content.Context;
 import android.content.res.AssetManager;
 
+import java.io.IOException;
+
 public class BeatBox {
 
     private static final String TAG = "BeatBox";
@@ -10,10 +12,21 @@ public class BeatBox {
 
     private AssetManager mAssets;
 
-    public BeatBox (Context context){
+    public BeatBox(Context context) {
         mAssets = context.getAssets();
+        loadSounds();
     }
 
+    private void loadSounds() {
+        String[] soundNames;
+        try {
+            soundNames = mAssets.list(SOUNDS_FOLDER);
+
+
+        } catch (IOException e) {
+            return;
+        }
+    }
 
 
 }
